6fe6e819bfa428c7e1cb32af874fc6fcef56e919
Alex Grieco <alexgrieco@Alexs-MacBook-Air.local>
2021-04-18 16:40:14 -0700

    Fixing user-data scripts

--- terraform/modules/hashistack/templates/user-data-client.sh
+++ terraform/modules/hashistack/templates/user-data-client.sh
@@ -3,6 +3,6 @@
 set -e
 
 exec > >(sudo tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
-sudo chmod +x /ops/scripts/client.sh
+sudo chmod +x /ops/scripts/start-client.sh
 sudo bash -c "NOMAD_BINARY=${nomad_binary} CONSUL_BINARY=${consul_binary} /ops/scripts/start-client.sh \"${node_class}\" \"${retry_join}\""
 rm -rf /ops/
--- terraform/modules/hashistack/templates/user-data-server.sh
+++ terraform/modules/hashistack/templates/user-data-server.sh
@@ -3,6 +3,6 @@
 set -e
 
 exec > >(sudo tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
-sudo chmod +x /ops/scripts/server.sh
+sudo chmod +x /ops/scripts/start-server.sh
 sudo bash -c "NOMAD_BINARY=${nomad_binary} CONSUL_BINARY=${consul_binary} /ops/scripts/start-server.sh \"${server_count}\" \"${retry_join}\""
 rm -rf /ops/
